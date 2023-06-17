35fbe44de068498b654206eed6397e7ae4f51e6d
Marcoroma02 <marcoromanella998@gmail.com>
2023-06-17 19:26:06 +0200

    Code cleaning and added some functions

--- functions.py
+++ functions.py
@@ -1,73 +1,83 @@
 # Import the libraries
 import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)
-import re  # regular expressions
-from urllib.parse import urlparse, urlunparse  # url parsing
 import numpy as np  # linear algebra
-import matplotlib.pyplot as plt  # data visualization
 import seaborn as sns  # statistical data visualization
-from sklearn.model_selection import train_test_split  # data split
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import GaussianNB, MultinomialNB
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
-import nltk
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score, confusion_matrix
-from sklearn.linear_model import SGDClassifier
-import validators
-from sklearn.model_selection import cross_val_score
 from tabulate import tabulate
+import matplotlib.pyplot as plt
+import os
 
 # RIGHE DA ELIMINARE: 521009
 
 """
     Funzione che stampa i dati
 """
-
-
 def print_data():
+
     print("SONO IN PRINT DATA")
     # Read the data
-    data = pd.read_csv('malicious_phish.csv', nrows=4000)
-
+    data = pd.read_csv('malicious_phish.csv') #nrows=50000
+    data.drop(index=521009)
     # Print the first 5 rows
+    print("Data head:")
     print(data.head())
-    print("--------------------")
+    print("--------------------\n\n")
+    # Print the data info
+    print("Data info:")
     print(data.info())
-    print("--------------------")
+    print("--------------------\n\n")
+    # Print 10 data sample
+    print("Data sample:")
     print(data.sample(10))
-    print("--------------------")
+    print("--------------------\n\n")
+    # Print the data shape
+    print("Data shape:")
     print(data.shape)
-    print("--------------------")
+    print("--------------------\n\n")
+    # Print the data describe
+    print("Data describe:")
     print(data.describe())
-    print("--------------------")
+    print("--------------------\n\n")
     clean_data(data)
 
 
 """
     Funzione che pulisce i dati
 """
-
-
 def clean_data(data):
     print("SONO IN CLEAN DATA")
-    data['type'] = data['type'].values.astype(str)
-    print(data['type'])
-    print("Drop duplicates and NA values")
-    # data = data.drop_duplicates()
-    # print("Dropna")
-    # data = data.dropna()
-    # print("Validators")
-    # data = data[data['url'].apply(lambda x: validators.url(x))]
-    print("--------------------")
+    #print("Data types: ")
+    #data['type'] = data['type'].values.astype(str)
+    #print(data['type'])
+    #print("--------------------\n\n")
+
+    # Delete the duplicates and NA values
+    print("Drop duplicates and NA values: ")
+    data = data.drop_duplicates()
+    print("--------------------\n\n")
+    # Delete the rows with NA values
+    print("Dropna: ")
+    data = data.dropna()
+    print("--------------------\n\n")
+    # Print the data shape
+    print("Data shape:")
+    print(data.shape)
+    print("--------------------\n\n")
+    # Print the data describe
+    print("Data describe:")
+    print(data.describe())
+    print("--------------------\n\n")
     data_discovery(data)
 
 
 """
     Funzione che esegue la data discovery
 """
-
-
 def data_discovery(data):
     print("SONO IN DATA DISCOVERY")
 
@@ -77,32 +87,52 @@ def data_discovery(data):
         return round(pct, 2)
 
     # Group by type
+    print("Group by type: ")
     grouped_data = data.groupby('type').size()
     print(grouped_data)
+    print("--------------------\n\n")
 
     d_benign = grouped_data['benign']
     d_phishing = grouped_data['phishing']
     d_defacement = grouped_data['defacement']
     d_malware = grouped_data['malware']
 
+    # Print the percentual of each type
+    print("Percentual of each type: ")
     print('Benign:', d_benign, '%', perc(d_benign))
     print('Phishing:', d_phishing, '%', perc(d_phishing))
     print('Defacement:', d_defacement, '%', perc(d_defacement))
     print('Malware:', d_malware, '%', perc(d_malware))
+    print("--------------------\n\n")
 
     # Plot the data
     p_bar = data['type'].value_counts().plot(kind='bar', title='Type', figsize=(10, 8));
     p_bar.set_ylabel('Count');
+
+    # Save the plot
+    folder_path_info = '/Users/marcoromanella/Desktop/MaliciousUrls/img/info'
+    file_name_info = 'type.png'
+    file_path_info = os.path.join(folder_path_info, file_name_info)
+    plt.savefig(file_path_info, dpi=300, bbox_inches='tight')
     plt.show()
+
+
+    # Grafico a torta
+    data['type'].value_counts().plot.pie(autopct='%1.1f%%')
+    folder_path_info = '/Users/marcoromanella/Desktop/MaliciousUrls/img/info'
+    file_name_info = 'pie.png'
+    file_path_info = os.path.join(folder_path_info, file_name_info)
+    plt.savefig(file_path_info, dpi=300, bbox_inches='tight')
+    plt.show()
+
     print("--------------------")
+
     data_preprocessing(data)
 
 
 """
     Funzione che pre-elabora i dati
 """
-
-
 def data_preprocessing(data):
     print("SONO IN DATA PREPROCESSING")
     """
@@ -133,15 +163,22 @@ def training_and_testing(data):
     print("SONO IN TRAINING AND TESTING")
 
     # Dimensione del batch
-    batch_size = 1000
-
+    batch_size = 10000
     total_raws = len(data)
 
     models = [
-        DecisionTreeClassifier(),
-        RandomForestClassifier(),
-        LogisticRegression(),
-        SGDClassifier()
+        MultinomialNB(),           # FUNZIONA
+        GaussianNB(),              # FUNZIONA
+        DecisionTreeClassifier(),  # FUNZIONA
+        RandomForestClassifier(),  # FUNZIONA
+
+        #KNeighborsClassifier(),    # FUNZIONA MA PESANTE
+        #MLPClassifier(),           # PESANTE
+        ##SupportVectorMachine(),   # NON FUNZIONA
+        ##LogisticRegression(),     # NON FUNZIONA
+        ##CatBoostClassifier(),     # NON FUNZIONA
+        ##LogisticRegression(),     # NON FUNZIONA
+        ##SGDClassifier()           # NON FUNZIONA
     ]
 
     results = []
@@ -215,17 +252,20 @@ def training_and_testing(data):
 
         salva_risultati(results, model, avarage_accuracy, avarage_precision, avarage_recall, avarage_f1)
 
+        """
         print("Accuracy: ", avarage_accuracy)
         print("Precision: ", avarage_precision)
         print("Recall: ", avarage_recall)
         print("F1: ", avarage_f1)
-
+        """
         crea_tabella(model, avarage_accuracy, avarage_precision, avarage_recall, avarage_f1)
         crea_confusion_matrix(model, total_labels, total_predictions)
 
-        print("\n--------------------\n\n")
+    mostra_risultati(results)
+    print("\n--------------------\n\n")
 
 
+# Creazione tabella in formato png dei vari risultati
 def crea_tabella(model, avarage_accuracy, avarage_precision, avarage_recall, avarage_f1):
     print("SONO IN CREA TABELLA")
     # Creazione della tabella
@@ -248,13 +288,16 @@ def crea_tabella(model, avarage_accuracy, avarage_precision, avarage_recall, ava
     # Centrare l'immagine nel grafico
     fig.tight_layout(pad=3)
 
-    # Salvataggio della tabella come immagine PNG
-    plt.savefig('tabella.png', bbox_inches='tight', pad_inches=0.5, dpi=300)
+    folder_path_table = '/Users/marcoromanella/Desktop/MaliciousUrls/img/table'
+    file_name_table = f'{model.__class__.__name__}Table.png'
+    file_path_table = os.path.join(folder_path_table, file_name_table)
+    plt.savefig(file_path_table, bbox_inches='tight', dpi=300)
 
     # Mostra la tabella
     plt.show()
 
 
+# Creazione della matrice di confusione
 def crea_confusion_matrix(model, total_labels, total_predictions):
     confusion = confusion_matrix(total_labels, total_predictions)
     plt.figure(figsize=(8, 6))
@@ -262,12 +305,83 @@ def crea_confusion_matrix(model, total_labels, total_predictions):
     plt.title(model.__class__.__name__)
     plt.xlabel('Predicted Label')
     plt.ylabel('True Label')
+
+    # Salvataggio della matrice di confusione
+    folder_path_confusion_matrix = '/Users/marcoromanella/Desktop/MaliciousUrls/img/confusion_matrix'
+    file_name_confusion_matrix = f'{model.__class__.__name__}ConfusionMatrix.png'
+    file_path_confusion_matrix = os.path.join(folder_path_confusion_matrix, file_name_confusion_matrix)
+    plt.savefig(file_path_confusion_matrix, bbox_inches='tight', dpi=300)
     plt.show()
 
 
+# Salvataggio dei risultati in un dizionario
 def salva_risultati(results, model, avarage_accuracy, avarage_precision, avarage_recall, avarage_f1):
     results.append({'Model': model.__class__.__name__,
                     'Accuracy': avarage_accuracy,
                     'Precision': avarage_precision,
                     'Recall': avarage_recall,
                     'F1': avarage_f1})
+
+
+def mostra_risultati(results):
+    print("SONO IN MOSTRA RISULTATI")
+    table_results = pd.DataFrame(results)
+    print(table_results.head())
+
+
+    table_results.plot.bar(x='Model', y='Accuracy', legend=False)
+    plt.xlabel('Model')
+    plt.ylabel('Accuracy')
+    plt.title('Accuracy of Different Models')
+    #plt.xticks(rotation=45)
+    folder_path_results = '/Users/marcoromanella/Desktop/MaliciousUrls/img/results'
+    file_name_results = 'accuracy.png'
+    file_path_results = os.path.join(folder_path_results, file_name_results)
+    plt.savefig(file_path_results, bbox_inches='tight', dpi=300)
+    plt.show()
+
+
+    table_results.plot.bar(x='Model', y='Precision', legend=False)
+    plt.xlabel('Model')
+    plt.ylabel('Precision')
+    plt.title('Precision of Different Models')
+    #plt.xticks(rotation=45)
+    folder_path_results = '/Users/marcoromanella/Desktop/MaliciousUrls/img/results'
+    file_name_results = 'precision.png'
+    file_path_results = os.path.join(folder_path_results, file_name_results)
+    plt.savefig(file_path_results, bbox_inches='tight', dpi=300)
+    plt.show()
+
+    table_results.plot.bar(x='Model', y='Recall', legend=False)
+    plt.xlabel('Model')
+    plt.ylabel('Recall')
+    plt.title('Recall of Different Models')
+    #plt.xticks(rotation=45)
+    folder_path_results = '/Users/marcoromanella/Desktop/MaliciousUrls/img/results'
+    file_name_results = 'recall.png'
+    file_path_results = os.path.join(folder_path_results, file_name_results)
+    plt.savefig(file_path_results, bbox_inches='tight', dpi=300)
+    plt.show()
+
+    table_results.plot.bar(x='Model', y='F1', legend=False)
+    plt.xlabel('Model')
+    plt.ylabel('F1')
+    plt.title('F1 of Different Models')
+    #plt.xticks(rotation=45)
+    folder_path_results = '/Users/marcoromanella/Desktop/MaliciousUrls/img/results'
+    file_name_results = 'f1.png'
+    file_path_results = os.path.join(folder_path_results, file_name_results)
+    plt.savefig(file_path_results, bbox_inches='tight', dpi=300)
+    plt.show()
+
+    table_results.plot.bar(x='Model', y=['Accuracy', 'Precision', 'Recall', 'F1'], legend=False)
+    plt.xlabel('Model')
+    plt.ylabel('Accuracy, Precision, Recall, F1')
+    plt.title('Accuracy, Precision, Recall, F1 of Different Models')
+    #plt.xticks(rotation=45)
+    folder_path_results = '/Users/marcoromanella/Desktop/MaliciousUrls/img/results'
+    file_name_results = 'accPreRecF1.png'
+    file_path_results = os.path.join(folder_path_results, file_name_results)
+    plt.savefig(file_path_results, bbox_inches='tight', dpi=300)
+    plt.show()
+
